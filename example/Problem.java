f88c8450bad7ef672dc2336d8bd992811ebfb9fc
cjk <cjk@uber.com>
2022-05-06 20:52:30 +0000

    Add comment

--- example/Problem.java
+++ example/Problem.java
@@ -8,6 +8,7 @@ public class Problem {
 
   public void test() throws URISyntaxException {
     Response res = Response.created(new URI("test")).build();
+    // commenting out the following line allows the build to succeed
     res.readEntity(new GenericType<>() {});
   }
 }
